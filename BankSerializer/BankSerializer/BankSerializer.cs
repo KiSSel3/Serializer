afeae0527eca729dac21dd7638cbe85316dfe63f
KiSSel69 <andreyk042004@gmail.com>
2023-05-09 20:08:16 +0300

    Added JSON serialize

---
+++ BankSerializer/BankSerializer/BankSerializer.cs
@@ -0,0 +1,19 @@
+﻿using BankServer.Models;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace BankSerializer
+{
+    public class BankSerializer
+    {
+        public string SerializeJSON<T>(IEnumerable<T> users)
+        {
+            return JsonSerializer.Serialize(users);
+        }
+
+        public IEnumerable<T> DeSerializeJSON<T>(string data)
+        {
+            return JsonSerializer.Deserialize(data, typeof(IEnumerable<T>)) as IEnumerable<T>;
+        }
+    }
+}
---
+++ BankSerializer/TestSerializer/Program.cs
@@ -0,0 +1,32 @@
+﻿using BankSerializer;
+using BankServer.Models;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankSerializer.BankSerializer bankSerializer = new();
+
+        /*        List<UserModel> users = new List<UserModel>()
+                {
+                    new UserModel(1, "Andrey","123"),
+                    new UserModel(2, "Nastya", "123"),
+                };*/
+
+        List<InvoiceModel> users = new List<InvoiceModel>()
+        {
+            new InvoiceModel(new UserModel(1, "Andrey","123"), 1, "123", 100),
+        };
+        var str = bankSerializer.SerializeJSON<InvoiceModel>(users);
+        Console.WriteLine(str);
+
+        var list = bankSerializer.DeSerializeJSON<InvoiceModel>(str);
+        foreach(var item in list)
+        {
+            Console.WriteLine(item.Balanse);
+        }
+
+
+
+
+    }
+}
